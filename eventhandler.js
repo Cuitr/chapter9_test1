2f8a46c4952a6810c4b426691c58948f90542445
Vinh <tranthanhvinhlxag1995@gmail.com>
2023-07-23 00:44:58 +0700

    Add new feture

--- eventhandler.js
+++ eventhandler.js
@@ -3,11 +3,18 @@ function blurImage(){
     let blur = document.getElementsByTagName("img");
     for(let i = 0; i < blur.length; i++){
         blur[i].onclick = unblurImage;//click event handler
-    }
+    
 }
 function unblurImage(eventObj){
     let unblur = eventObj.target;
     let name = unblur.id;
     name = name + ".jpg";
     unblur.src = name;
+    setTimeout(reblur, 2000, unblur);
+}
+function reblur(unblur){
+    let name = unblur.id;
+    name = name + "blur.jpg";
+    unblur.src = name;
+}
 }
